3c1b7f2c8ace8cbaa2e4ed4336725c0a674d03d9
ralph john <bossrj12@gmail.com>
2026-05-22 06:17:06 +0800

    Redirect root URL to client landing page.

--- src/Controller/Client/LandingController.php
+++ src/Controller/Client/LandingController.php
@@ -3,11 +3,18 @@
 namespace App\Controller\Client;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LandingController extends AbstractController
 {
+    #[Route('/', name: 'app_root', methods: ['GET'])]
+    public function root(): RedirectResponse
+    {
+        return $this->redirectToRoute('client_landing', [], Response::HTTP_FOUND);
+    }
+
     #[Route('/client', name: 'client_landing')]
     #[Route('/client/dashboard', name: 'app_client_dashboard')]
     public function index(): Response
